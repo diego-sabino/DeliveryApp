50498920bd560e7e0fba8f0b8940265cff0ca7d8
AlanoBurity <alanosantosdev@gmail.com>
2023-01-20 16:16:41 -0300

    criação migration products

---
+++ back-end/src/database/migrations/products.migration.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+
+
+const urlDefault = 'https://bityli.com/XDCXy'
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("products", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            price: {
+                allowNull: false,
+                type: Sequelize.DECIMAL(4,2),
+            },
+            urlImage: {
+                type: Sequelize.STRING,
+                field: 'url_image',
+                default: urlDefault
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("products");
+    },
+};
