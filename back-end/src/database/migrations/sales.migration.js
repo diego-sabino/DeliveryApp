7afdcc8344844408c3d0f016e62b82666de325f3
AlanoBurity <alanosantosdev@gmail.com>
2023-01-20 16:45:42 -0300

    criando migration sales

---
+++ back-end/src/database/migrations/sales.migration.js
@@ -0,0 +1,62 @@
+const Sequelize = require("sequelize");
+
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("sales", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+                foreingKey: true,
+                field: 'user_id'
+            },
+            sellerId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+                foreingKey: true,
+                field: "seller_id"
+            },
+            totalPrice: {
+                type: Sequelize.DECIMAL(9.2),
+                field: 'total_price',
+            },
+            deliveryAddress: {
+                type: Sequelize.STRING,
+                field: 'delivery_adress'
+            },
+            deliveryNumber: {
+                type: Sequelize.STRING,
+                field: 'delivery_number'
+            },
+            saleDate: {
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('now'),
+                field: 'sale_date'
+            },
+            status: {
+                type: Sequelize.STRING,
+            }, 
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("sales");
+    },
+};
