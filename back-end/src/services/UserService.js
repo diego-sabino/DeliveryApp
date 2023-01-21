c88937b92badad2a2380fec25ff8fa4702459dfb
adyluna <adylunaleite@gmail.com>
2023-01-21 11:26:18 -0300

    backend: user endpoint: criando service

---
+++ back-end/src/services/UserService.js
@@ -0,0 +1,49 @@
+const User = require('../database/models/User')
+
+const findUserById = async (id) => {
+  const result = await User.findByPk(id);
+  
+  if (result) {
+    delete result.dataValues.password;
+  }
+
+  return result;
+};
+
+const findUser = async (email) => {
+  const singleUser = await User.findOne({ where: { email } });
+
+  if (!singleUser) {
+    return { type: 'USER_NOT_FOUND', message: 'Invalid fields' };
+  }
+
+  return { type: null, message: singleUser };
+};
+
+const findAllUsers = async () => {
+  const users = await User.findAll();
+  const usersHandler = users.map((user) => user.dataValues);
+  usersHandler.forEach((elem) => {
+    const element = elem;
+    delete element.password;
+  });
+
+  return { type: null, message: usersHandler };
+};
+
+const createUser = async (user) => {
+  const newUser = await User.create({ ...user });
+
+  if (!newUser) {
+    return { type: 'CREATE_USER_FAIL', message: 'User already exists' };
+  }
+
+  return { type: null, message: newUser };
+};
+
+module.exports = {
+  findUser,
+  createUser,
+  findAllUsers,
+  findUserById,
+};
