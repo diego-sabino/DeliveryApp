207be594f9356450168889c807871bf95a1be2ec
adyluna <adylunaleite@gmail.com>
2023-01-23 15:25:48 -0300

    docker: criado docker-compose

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,8 +4,6 @@ const UserRoutes = require('../routes/UserRoutes');
 
 const app = express();
 
-// Testando dois!!
-
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
