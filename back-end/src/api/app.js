020a9162a2c3f028728456c60d2c77020ba69ed0
adyluna <adylunaleite@gmail.com>
2023-01-23 16:17:53 -0300

    backend: cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+// app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
